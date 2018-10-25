b3f7e1a16ad246de6b4be74742609aa74b8edc55
Elijah-P <42383409+Elijah-P@users.noreply.github.com>
2018-10-25 08:46:05 -0700

    Started MadLibs

---
+++ notes/Elijah Palacios - Mad Lib.py
@@ -0,0 +1,15 @@
+# 10 words being taken
+num = input("Pick a number")
+adj1 = input("Pick a place")
+
+
+
+
+
+
+print("For like" + num + " years me and my family always wanted me to go to this school,so I had to live up to their")
+print("expectations. They always bragged about how good it was to come to the school. They said that there were a lot")
+print("of clubs and that I could do anything I wanted to do. My mom and dad did go so I believed them. They did meet")
+print("at the school. So on a regular day, my mom checked the mailbox and it was a letter from Computech!")
+print("This was it. Either I got accepted to Computech or I didn't make it. I made it! It was one of the greatest")
+print("moments of my life!")
