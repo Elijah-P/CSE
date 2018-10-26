496964ee073cbd2e12c683cb629185b58aa1ec33
Elijah-P <42383409+Elijah-P@users.noreply.github.com>
2018-10-26 09:42:25 -0700

    finished MadLibs

--- notes/Elijah Palacios - Mad Lib.py
+++ notes/Elijah Palacios - Mad Lib.py
@@ -1,15 +1,19 @@
 # 10 words being taken
 num = input("Pick a number")
-adj1 = input("Pick a place")
+noun = input("Pick a place")
+verb = input("Pick a verb")
+noun2 = input("Pick a object")
+verb2 = input("pick something to do")
+verb3 = input("pick something to do")
+type = input("Pick a type of day")
+check = input("Pick something to check")
+noun3 = input("Pick another object")
+noun4 = input("Pick one last thing")
 
 
-
-
-
-
-print("For like" + num + " years me and my family always wanted me to go to this school,so I had to live up to their")
-print("expectations. They always bragged about how good it was to come to the school. They said that there were a lot")
-print("of clubs and that I could do anything I wanted to do. My mom and dad did go so I believed them. They did meet")
-print("at the school. So on a regular day, my mom checked the mailbox and it was a letter from Computech!")
-print("This was it. Either I got accepted to Computech or I didn't make it. I made it! It was one of the greatest")
-print("moments of my life!")
+print("For like " +num+ " years me and my family always wanted me to go to "+noun+",so I had to live up to their")
+print("expectations. They were always "+verb+" about how good it was to come to "+noun+". They said that there were a lot")
+print("of "+noun2+"s and that I could do "+verb2+". My mom and dad did "+verb3+" so I believed them. They did")
+print("meet at "+noun+". So on a "+type+" day, my mom checked the "+check+" and it was a "+noun3+" from "+noun+"!")
+print("This was it. Either I got accepted to "+noun+" or I didn't make it. I made it! It was one of the greatest ")
+print(" "+noun4+" of my life!")
