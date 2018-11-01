da2e60fe2a0d7f75107a5420034467f3bc2439fe
Elijah-P <42383409+Elijah-P@users.noreply.github.com>
2018-11-01 08:45:14 -0700

    Almost done

--- notes/Basic Python Notes.py
+++ notes/Basic Python Notes.py
@@ -118,3 +118,13 @@ def grade_calc(percentage):
 
 
 print(grade_calc(62))
+
+# Equality Statements
+print(5 > 3)
+print(5 >= 3)
+print(3 == 3)
+print(3 != 4)
+"""
+a = 3 # A is set to equal 3
+a == 3 # Is a equal to 3?
+"""
---
+++ notes/Elijah Palacios - Guess Game.py
@@ -0,0 +1,11 @@
+import random
+a = random.randint(1, 10)
+
+for i in (1, 2, 3, 4, 5):
+    answer = int(input("Pick a number between 1 and 10"))
+    if answer > a:
+        print("The answer is a lower number")
+    if answer < a:
+        print("The number is higher")
+    if answer == a:
+        print("You got the right number")
