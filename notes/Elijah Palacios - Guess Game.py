638f4c0e052d14d07dcc006df1128e0d50289ea2
Elijah-P <42383409+Elijah-P@users.noreply.github.com>
2018-11-02 08:45:23 -0700

    Changed loop to while

--- notes/Elijah Palacios - Guess Game.py
+++ notes/Elijah Palacios - Guess Game.py
@@ -1,11 +1,15 @@
 import random
 a = random.randint(1, 10)
-
-for i in (1, 2, 3, 4, 5):
-    answer = int(input("Pick a number between 1 and 10"))
-    if answer > a:
-        print("The answer is a lower number")
-    if answer < a:
-        print("The number is higher")
+loop = 0
+#  for i in (1, 2, 3, 4, 5):
+answer = int(input("Pick a number between 1 and 10 "))
+if answer > a:
+    print("The answer is a lower number")
+if answer < a:
+    print("The number is higher")
     if answer == a:
         print("You got the right number")
+    if i == 5:
+        print("You lose")
+
+        print("You win!")
