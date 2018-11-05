2fa71d7ae9f8d9acbe96fef62bec2594f76b3575
Elijah-P <42383409+Elijah-P@users.noreply.github.com>
2018-11-05 08:44:19 -0800

    trying to end loop

--- notes/Elijah Palacios - Guess Game.py
+++ notes/Elijah Palacios - Guess Game.py
@@ -1,15 +1,18 @@
 import random
 a = random.randint(1, 10)
 loop = 0
-#  for i in (1, 2, 3, 4, 5):
-answer = int(input("Pick a number between 1 and 10 "))
-if answer > a:
-    print("The answer is a lower number")
-if answer < a:
-    print("The number is higher")
-    if answer == a:
+while loop != 5:
+    answer = int(input("Pick a number between 1 and 10 "))
+    if answer > a:
+        print("The answer is a lower number")
+        loop += 1
+    elif answer < a:
+        print("The number is higher")
+        loop += 1
+    elif answer == a:
         print("You got the right number")
-    if i == 5:
-        print("You lose")
-
-        print("You win!")
+        loop = 5
+    if loop == 5:
+        print("You Win!")
+    if loop == 5:
+            print("You lose")
