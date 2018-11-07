e1813ed06eebe8ad5c3b2a8236e944e2c4a61f91
Elijah-P <42383409+Elijah-P@users.noreply.github.com>
2018-11-07 07:52:01 -0800

    Revert "Changed loop to while"
    This reverts commit 638f4c0e052d14d07dcc006df1128e0d50289ea2.

--- notes/Elijah Palacios - Guess Game.py
+++ notes/Elijah Palacios - Guess Game.py
@@ -1,5 +1,6 @@
 import random
 a = random.randint(1, 10)
+<<<<<<< HEAD
 loop = 0
 while loop != 5:
     answer = int(input("Pick a number between 1 and 10 "))
@@ -14,3 +15,14 @@ while loop != 5:
         print("You Win!")
     elif loop == 5:
         print("You lose")
+=======
+
+for i in (1, 2, 3, 4, 5):
+    answer = int(input("Pick a number between 1 and 10"))
+    if answer > a:
+        print("The answer is a lower number")
+    if answer < a:
+        print("The number is higher")
+    if answer == a:
+        print("You got the right number")
+>>>>>>> parent of 638f4c0... Changed loop to while
