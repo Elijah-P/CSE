3e386e49acc6e8efb60097c5185295b4494518a7
Elijah-P <42383409+Elijah-P@users.noreply.github.com>
2018-11-07 08:29:12 -0800

    Finished Guess Game

--- notes/Elijah Palacios - Guess Game.py
+++ notes/Elijah Palacios - Guess Game.py
@@ -1,28 +1,20 @@
 import random
 a = random.randint(1, 10)
-<<<<<<< HEAD
-loop = 0
-while loop != 5:
+win = False
+guesses_left = 5
+while guesses_left > 0 and not win:
     answer = int(input("Pick a number between 1 and 10 "))
-    if answer > a:
-        print("The answer is a lower number")
-        loop += 1
-    elif answer < a:
-        print("The number is higher")
-        loop += 1
-    elif answer == a:
-        print("You got the right number")
-        print("You Win!")
-    elif loop == 5:
-        print("You lose")
-=======
-
-for i in (1, 2, 3, 4, 5):
-    answer = int(input("Pick a number between 1 and 10"))
-    if answer > a:
-        print("The answer is a lower number")
-    if answer < a:
-        print("The number is higher")
     if answer == a:
         print("You got the right number")
->>>>>>> parent of 638f4c0... Changed loop to while
+        print("You Win!")
+        win = True
+    else:
+        guesses_left -= 1
+    if answer > a:
+        print("The answer is a lower number")
+    elif answer < a:
+        print("The number is higher")
+    if guesses_left == 0:
+        print("You lose")
+
+
---
+++ notes/Elijah Palacios - Lucky 7s.py
@@ -0,0 +1,6 @@
+import random
+Money = 15
+Rounds = 0
+Dice1 = random.randint(1, 6)
+Dice2 = random.randint(1, 6)
+
