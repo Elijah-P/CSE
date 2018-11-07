71f60a5abc84a1d3b214ff8a087ab3021cf2fa51
Elijah-P <42383409+Elijah-P@users.noreply.github.com>
2018-11-07 08:45:34 -0800

    Started Lucky 7s

--- notes/Elijah Palacios - Lucky 7s.py
+++ notes/Elijah Palacios - Lucky 7s.py
@@ -3,4 +3,10 @@ Money = 15
 Rounds = 0
 Dice1 = random.randint(1, 6)
 Dice2 = random.randint(1, 6)
+begin_game = input("Hello ready to gamble?")
+
+if begin_game == "yes" or "Yes" or "Yea":
+     print("Ok lets start!")
+
+
 
