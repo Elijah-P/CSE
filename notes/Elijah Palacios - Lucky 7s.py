d1f3eb67604310308969ddbc3a09ac43a0e3c3ed
Elijah-P <42383409+Elijah-P@users.noreply.github.com>
2018-11-13 08:45:29 -0800

    Got an idea

--- notes/Elijah Palacios - Lucky 7s.py
+++ notes/Elijah Palacios - Lucky 7s.py
@@ -1,9 +1,15 @@
+import timeit
 import random
 Money = 15
 Rounds = 0
 print("DOESN't matter what your response was, lets start!")
-maxMoney = list
+# maxMoney_list = list()  # don't use list, find something better.
+Maxmoney = 0
+
+timer = timeit.Timer()  # timing how long it takes to find maximum amount of money
+
 while Money > 0:
+    # maxMoney_list.append(Money)
     Dice1 = random.randint(1, 6)
     Dice2 = random.randint(1, 6)
     Money -= 1
@@ -11,8 +17,12 @@ while Money > 0:
     if total == 7:
         Money += 5
         Rounds += 1
+        if Money
     else:
         Money += 0
         Rounds += 1
     print("You have $%d" % Money)
 print("You lasted %d rounds" % Rounds)
+# Maxmoney = max(maxMoney_list)
+print("The maximum amount of money you had was $%d" % Maxmoney)
+print(timeit.timeit())
