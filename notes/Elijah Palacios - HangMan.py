5f3423d7569b184d1d3fd9bdcbec6a8b879c8d12
Elijah-P <42383409+Elijah-P@users.noreply.github.com>
2018-11-15 08:42:17 -0800

    Started HangMan

---
+++ notes/Elijah Palacios - HangMan.py
@@ -0,0 +1,5 @@
+import random
+word = ["HangMan", "School", "Math", "Physical Education", "Name", "Freshmen", "Sophomore", "Junior", "Senior",
+        "College", "Food", "Pizza", "Kerry", "Delaney", "Mr.Wiebe", "Smashbros", "Dancing", "Computers", "United"]
+random.choice(word)
+
