6bf62f4c3cf313b52f849dce0f106c55e104e0d1
Elijah-P <42383409+Elijah-P@users.noreply.github.com>
2018-11-16 08:43:47 -0800

    Added 2 lines of code

--- notes/Elijah Palacios - HangMan.py
+++ notes/Elijah Palacios - HangMan.py
@@ -1,5 +1,6 @@
 import random
 word = ["HangMan", "School", "Math", "Physical Education", "Name", "Freshmen", "Sophomore", "Junior", "Senior",
         "College", "Food", "Pizza", "Kerry", "Delaney", "Mr.Wiebe", "Smashbros", "Dancing", "Computers", "United"]
-random.choice(word)
+chosen_word = random.choice(word)
 
+for char in word
