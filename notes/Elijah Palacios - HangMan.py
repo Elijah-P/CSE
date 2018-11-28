86429f5f06106cca19a3c1e552d4ffc6a6ef645d
Elijah-P <42383409+Elijah-P@users.noreply.github.com>
2018-11-28 08:44:38 -0800

    Was able to spell out chosen word

--- notes/Elijah Palacios - HangMan.py
+++ notes/Elijah Palacios - HangMan.py
@@ -2,5 +2,7 @@ import random
 word = ["HangMan", "School", "Math", "Physical Education", "Name", "Freshmen", "Sophomore", "Junior", "Senior",
         "College", "Food", "Pizza", "Kerry", "Delaney", "Mr.Wiebe", "Smashbros", "Dancing", "Computers", "United"]
 chosen_word = random.choice(word)
+list_Chosen_word = list(chosen_word)
+print(list_Chosen_word)
 
-for char in word
+
