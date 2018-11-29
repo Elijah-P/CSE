df848fd7610fb248a3d87f6acf317c827b08eb40
Elijah-P <42383409+Elijah-P@users.noreply.github.com>
2018-11-29 08:44:14 -0800

    Figured out how to make a letter change

--- notes/Elijah Palacios - HangMan.py
+++ notes/Elijah Palacios - HangMan.py
@@ -1,8 +1,18 @@
 import random
-word = ["HangMan", "School", "Math", "Physical Education", "Name", "Freshmen", "Sophomore", "Junior", "Senior",
-        "College", "Food", "Pizza", "Kerry", "Delaney", "Mr.Wiebe", "Smashbros", "Dancing", "Computers", "United"]
+import string
+word = ["hangman", "school", "math", "physicaleducation", "name", "freshmen", "sophomore", "junior", "senior",
+        "college", "food", "pizza", "kerry", "delaney", "wiebe", "smashbros", "dancing", "computers", "united"]
 chosen_word = random.choice(word)
 list_Chosen_word = list(chosen_word)
+letters = string.ascii_lowercase
+users_pick = input("Pick a letter")
+
+for i in range(len(list_Chosen_word)):
+    
+
+for each_letter in list_Chosen_word:
+        if users_pick == each_letter:
+            current_index = list_Chosen_word.index(users_pick)
+            list_Chosen_word.pop(current_index)
+            list_Chosen_word.insert(current_index, "*")
 print(list_Chosen_word)
-
-
---
+++ notes/junk test file.py
@@ -0,0 +1,7 @@
+import string
+import random
+
+random.randint(1, 5)
+letters = string.ascii_lowercase
+list1 = list(letters)
+print(letters)
