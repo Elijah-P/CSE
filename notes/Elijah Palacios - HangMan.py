6c8ea00e1f5e6cde1660709bf1aa0a86693ac869
Elijah-P <42383409+Elijah-P@users.noreply.github.com>
2018-11-30 09:40:00 -0800

    Updated Hangman

--- notes/Elijah Palacios - HangMan.py
+++ notes/Elijah Palacios - HangMan.py
@@ -4,15 +4,18 @@ word = ["hangman", "school", "math", "physicaleducation", "name", "freshmen", "s
         "college", "food", "pizza", "kerry", "delaney", "wiebe", "smashbros", "dancing", "computers", "united"]
 chosen_word = random.choice(word)
 list_Chosen_word = list(chosen_word)
+hidden_word = []
 letters = string.ascii_lowercase
-users_pick = input("Pick a letter")
 
 for i in range(len(list_Chosen_word)):
-    
+    hidden_word.append("*")
+print(hidden_word)
 
-for each_letter in list_Chosen_word:
+for i in range(8):
+    for each_letter in list_Chosen_word:
+        users_pick = input("Pick a letter")
         if users_pick == each_letter:
-            current_index = list_Chosen_word.index(users_pick)
-            list_Chosen_word.pop(current_index)
-            list_Chosen_word.insert(current_index, "*")
-print(list_Chosen_word)
+            current_index = hidden_word.index(users_pick)
+            hidden_word.pop(current_index)
+            hidden_word.insert(current_index, "")
+            print(hidden_word)
