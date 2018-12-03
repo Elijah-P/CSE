2584cd7b44830f63dd2702516ed415403f9a7045
Elijah-P <42383409+Elijah-P@users.noreply.github.com>
2018-12-03 08:45:36 -0800

    Started doing challenges

---
+++ notes/Elijah Palacios - Python Challenges.py
@@ -0,0 +1,20 @@
+import random
+def challenge1(firstname, lastname):
+    print(lastname, firstname)
+challenge1("Elijah", "Palacios")
+
+def challenge3(area)
+    print(base*height / 2)
+base = random integer
+
+def challenge7(number):
+    print(number+number*number+number**3)
+challenge7(2)
+string1 = "word"
+
+def challenge8(number):
+    if number >= 150 and number <= 2000:
+        print("This number is valid and works")
+    else:
+        print("This number does not work")
+challenge8(150)
