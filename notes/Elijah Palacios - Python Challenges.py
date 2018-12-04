2112122b5153deafcb53f8590f3c6db8433edcd5
Elijah-P <42383409+Elijah-P@users.noreply.github.com>
2018-12-04 08:43:02 -0800

    Finished more python challenges

--- notes/Elijah Palacios - Python Challenges.py
+++ notes/Elijah Palacios - Python Challenges.py
@@ -1,16 +1,44 @@
-import random
 def challenge1(firstname, lastname):
     print(lastname, firstname)
 challenge1("Elijah", "Palacios")
+print()
 
-def challenge3(area)
+
+def challenge2(given_number):
+    if given_number % 2 == 0:
+        print("This number is an even number")
+    else:
+        print("This number is an odd number")
+challenge2(int(input("pick a even or odd number, and I'll figure out which one it is.")))
+print()
+
+
+def challenge3(base, height):
     print(base*height / 2)
-base = random integer
+challenge3(5, 10)
+print()
+
+
+def challenge4(number):
+    if number > 0:
+        print("This is a positive number")
+    elif number < 0:
+        print("This is a negative number")
+    elif number == 0:
+        print("This is zero")
+
+
+def challenge5(radius):
+    print(3.14 * radius**2)
+challenge5(2)
+print()
+
 
 def challenge7(number):
     print(number+number*number+number**3)
 challenge7(2)
-string1 = "word"
+print()
+
 
 def challenge8(number):
     if number >= 150 and number <= 2000:
