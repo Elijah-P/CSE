58911fd1f36fba3567527ece949c2397424ceb17
Elijah-Palacios <42383409+Elijah-P@users.noreply.github.com>
2019-01-08 08:44:23 -0800

    Started new semester notes

---
+++ notes/Semester 2 Notes.py
@@ -0,0 +1,20 @@
+print("Hello World")
+# This is a single-line comment
+cars = 5
+driving = True
+
+
+print("I have %d cars" % cars)
+print("I have " + str(cars) + " cars")
+
+age = input("How old are you?")
+print("You're %s years old?! LMAOOOOOO" % age)
+
+# Make a list of at least 5 colors
+Colors_list = ["Red", "Purple", "Blue", "Green", "Yellow"]
+Colors_list.append("brown")
+
+print(Colors_list)
+
+Colors_list.pop(0)
+print(Colors_list)
