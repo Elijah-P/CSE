b6cc13ba7c8239b44591a93a4fa39467288f7a19
Elijah-Palacios <42383409+Elijah-P@users.noreply.github.com>
2019-01-16 08:42:27 -0800

    Actually made progress in Hangman

--- notes/Elijah Palacios - Hangman(1).py
+++ notes/Elijah Palacios - Hangman(1).py
@@ -1,22 +1,30 @@
 import random
 import string
-word = ["Hangman", "school", "math", "physicaleducation","Freshmen", "Sophomore", "Junior", "Senior", "college", "food",
-        "pizza", "Mr.Wiebe", "Smashbros", "dancing", "computers", "United States" "I am late for class"]
+word = ["Hangman", "school", "math", "physicaleducation", "Freshmen", "Sophomore", "Junior", "Senior", "college",
+        "pizza", "Mr.Wiebe", "Smashbros", "dancing", "computers", "United States", "I am late for class"]
 Guesses_Left = 8
 Chosen_Word = random.choice(word)
 list_Chosen_word = list(Chosen_Word)
 letters = list(string.ascii_lowercase)
-hidden_word = []
+
 Users_Guessed_letters = []
 
 while Guesses_Left > 0:
-    for letters_in_word in range(len(list_Chosen_word)):
-        hidden_word.append("_")
-        print(hidden_word)
-        users_pick = input("Pick a letter")
-        if users_pick == list_Chosen_word.index(0):
-            hidden_word.append(list_Chosen_word.index(0))
-            print(hidden_word)
-        Users_Guessed_letters.append(users_pick)
-        print(Users_Guessed_letters)
+    # Hides / Reveal Letters based on user guesses
+    hidden_word = []
+    for letter in list_Chosen_word:
+        if letter not in Users_Guessed_letters:
+            hidden_word.append("_ ")
+        else:
+            hidden_word.append(letter)
+    print("".join(hidden_word))
+
+    # Ask for input
+    users_pick = input("Pick a letter")
+    Users_Guessed_letters.append(users_pick)
+    print(Users_Guessed_letters)
+
+    # Check to see if the letters are in the word
+    if users_pick not in list_Chosen_word:
         Guesses_Left -= 1
+    
