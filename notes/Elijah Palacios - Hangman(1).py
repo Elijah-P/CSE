fbf49b5db3ea51af26703e56caa45f1657ed202e
Elijah-Palacios <42383409+Elijah-P@users.noreply.github.com>
2019-01-25 08:43:22 -0800

    99% done with Hangman

--- notes/Elijah Palacios - Hangman(1).py
+++ notes/Elijah Palacios - Hangman(1).py
@@ -1,22 +1,25 @@
 import random
-word = ["Hangman", "School", "Math", "Physical Education", "Freshmen", "Sophomore", "Junior", "Senior", "College",
-        "Pizza", "Mr.Wiebe", "Smashbros", "Dancing", "Computers", "United States", "I am late for class"]
+word = ["Hangman.", "School", "Math", "Physical Education", "Freshmen.", "Sophomore", "Junior", "Senior", "College",
+        "Pizza", "Mr.Wiebe", "Smashbros.", "Dancing", "Computers", "United States", "Am late for class?",
+        "U.S.A", "This is Mr.Wiebe's computer."]
 Guesses_Left = 8
 Chosen_Word = random.choice(word)
 list_Chosen_word = list(Chosen_Word)
 Users_Guessed_letters = []
+punctuation = [".", "'"]
 
 while Guesses_Left > 0:
     # Hides / Reveal Letters based on user guesses
     hidden_word = []
     for letter in list_Chosen_word:
-        if letter.lower() not in Users_Guessed_letters:
+        if letter.lower() not in Users_Guessed_letters and letter not in punctuation:
             hidden_word.append("_ ")
         else:
             hidden_word.append(letter)
 
     print("".join(hidden_word))
     if hidden_word == list_Chosen_word:
+        print("You win!")
         Guesses_Left = 0
         continue
 
@@ -28,3 +31,5 @@ while Guesses_Left > 0:
     # Check to see if the letters are in the word
     if users_pick not in list_Chosen_word:
         Guesses_Left -= 1
+        if Guesses_Left == 0:
+            print("You lose")
