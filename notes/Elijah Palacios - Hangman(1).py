4bb31017539d166199964a6017ec56bf29f44607
Elijah-Palacios <42383409+Elijah-P@users.noreply.github.com>
2019-01-28 08:34:25 -0800

    Finished Hangman

--- notes/Elijah Palacios - Hangman(1).py
+++ notes/Elijah Palacios - Hangman(1).py
@@ -1,4 +1,5 @@
 import random
+import string
 word = ["Hangman.", "School", "Math", "Physical Education", "Freshmen.", "Sophomore", "Junior", "Senior", "College",
         "Pizza", "Mr.Wiebe", "Smashbros.", "Dancing", "Computers", "United States", "Am late for class?",
         "U.S.A", "This is Mr.Wiebe's computer."]
@@ -6,7 +7,14 @@ Guesses_Left = 8
 Chosen_Word = random.choice(word)
 list_Chosen_word = list(Chosen_Word)
 Users_Guessed_letters = []
-punctuation = [".", "'"]
+punctuation = [".", "'", "?", " "]
+exclude_punctuation = []
+users_pick = ""
+
+# Build exclusive word
+for letter in list_Chosen_word:
+    if letter in string.ascii_letters + " ":
+        exclude_punctuation.append(letter)
 
 while Guesses_Left > 0:
     # Hides / Reveal Letters based on user guesses
@@ -17,11 +25,17 @@ while Guesses_Left > 0:
         else:
             hidden_word.append(letter)
 
-    print("".join(hidden_word))
     if hidden_word == list_Chosen_word:
+        print(Chosen_Word)
         print("You win!")
         Guesses_Left = 0
         continue
+    if users_pick.lower() == Chosen_Word.lower():
+        print(Chosen_Word)
+        print("You win!")
+        Guesses_Left = 0
+        continue
+    print("".join(hidden_word))
 
     # Ask for input
     users_pick = input("Pick a letter").lower()
