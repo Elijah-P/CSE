ce25833f38d3dfa6d4914933d8e4973161757a6f
Elijah-P <42383409+Elijah-P@users.noreply.github.com>
2019-05-09 08:45:57 -0700

    Updated Code

--- notes/Elijah Palacios - Total Profit of Sales.py
+++ notes/Elijah Palacios - Total Profit of Sales.py
@@ -1,43 +1,6 @@
 import csv
 
-different_items = {
-    "Fruits": {
-        "Total Profit":0,
-    },
-    "Clothes": {
-        "Total Profit":0,
-    },
-    "Meat": {
-        "Total Profit":0,
-    },
-    "Beverages": {
-        "Total Profit":0,
-    },
-    "Office Supplies": {
-        "Total Profit":0,
-    },
-    "Cosmetics": {
-        "Total Profit":0,
-    },
-    "Snacks": {
-        "Total Profit":0,
-    },
-    "Personal": {
-        "Total Profit":0,
-    },
-    "House Hold": {
-        "Total Profit":0,
-    },
-    "Vegetables": {
-        "Total Profit":0,
-    },
-    "Baby Food": {
-        "Total Profit":0,
-    },
-    "Cereal": {
-        "Total Profit":0
-    }
-}
+different_items = {}
 
 
 with open("Sales Records.csv", "r") as old_csv:
@@ -45,10 +8,26 @@ with open("Sales Records.csv", "r") as old_csv:
     print("Processing...")
 
     for row in reader:
-        old_number = row[13]  # String
-
+        if row[0] == "Region":
+            continue
+        old_number = float(row[13])  # String
+        # print(old_number)
         # Find out what type of item it is
         item_type = row[2]
-        # Add to different_items
-        different_items[item_type] += old_number
-        print(old_number)
+        # print(item_type)
+        # # Add to different_items
+        try:
+            different_items[item_type] += old_number
+            different_items[item_type] = round(different_items[item_type], 2)
+        except KeyError:
+            different_items[item_type] = old_number
+
+most_profit = max(different_items.values())
+
+
+for key, value in different_items.items():
+    print(key, end=": ")
+    print("${:,}".format(value))
+
+
+print("%s has made the most profit" % most_profit)
