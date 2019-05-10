c5e4e8ea46f583118a2b8752aae0776d4795707f
Elijah-P <42383409+Elijah-P@users.noreply.github.com>
2019-05-10 09:43:34 -0700

    Finishec Total Profit

--- notes/Elijah Palacios - Total Profit of Sales.py
+++ notes/Elijah Palacios - Total Profit of Sales.py
@@ -2,7 +2,6 @@ import csv
 
 different_items = {}
 
-
 with open("Sales Records.csv", "r") as old_csv:
     reader = csv.reader(old_csv)
     print("Processing...")
@@ -11,23 +10,18 @@ with open("Sales Records.csv", "r") as old_csv:
         if row[0] == "Region":
             continue
         old_number = float(row[13])  # String
-        # print(old_number)
-        # Find out what type of item it is
         item_type = row[2]
-        # print(item_type)
-        # # Add to different_items
         try:
             different_items[item_type] += old_number
             different_items[item_type] = round(different_items[item_type], 2)
         except KeyError:
             different_items[item_type] = old_number
+    most_profit = max(different_items.values())
 
-most_profit = max(different_items.values())
-
-
-for key, value in different_items.items():
-    print(key, end=": ")
-    print("${:,}".format(value))
-
+    for key, value in different_items.items():
+        if value == most_profit:
+            most_profit = key
+        print(key, end=": ")
+        print("${:,}".format(value))
 
 print("%s has made the most profit" % most_profit)
